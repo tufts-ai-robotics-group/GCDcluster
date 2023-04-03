8ce47bb9b1cf4c600c75db3481c8832a7c613301
PatrickFeeney <patrickfeeney1225@gmail.com>
2023-04-03 10:29:30 -0400

    Deep SSGMM

--- gcd_cluster/ss_gmm.py
+++ gcd_cluster/ss_gmm.py
@@ -12,11 +12,12 @@ class SSGMM(GaussianMixture):
         self.y_labeled = y_labeled.astype(int)
         self.X_unlabeled = None  # dummy value needed for sklearn validation
         self.n_labeled = X_labeled.shape[0]
+        # initialize from SS K-means++
         means_init = ss_kmeans_plusplus(
             X_labeled, y_labeled, X_unlabeled, n_components - len(np.unique(y_labeled)))
         super().__init__(n_components, covariance_type="spherical", means_init=means_init)
-        self.resp_labeled = np.zeros((self.n_labeled, self.n_components))
-        self.resp_labeled[np.arange(self.resp_labeled.shape[0]), self.y_labeled] = 1
+        # cache labeled responsibilities
+        self.resp_labeled = targets_to_resp(y_labeled, self.n_components)
         # ignore warning about taking log(0)
         with np.errstate(divide="ignore"):
             self.log_resp_labeled = np.log(self.resp_labeled)
@@ -37,3 +38,55 @@ class SSGMM(GaussianMixture):
         self.precisions_cholesky_ = gm._compute_precision_cholesky(
             self.covariances_, self.covariance_type
         )
+
+
+class DeepSSGMM(GaussianMixture):
+    def __init__(self, X_labeled, y_labeled, X_unlabeled, n_components, covariance: float):
+        # dummy value needed for sklearn validation
+        self.X_labeled = None
+        self.y_labeled = None
+        self.X_unlabeled = None
+        self.covariance = None
+        # initialize from SS K-means++
+        means_init = ss_kmeans_plusplus(
+            X_labeled, y_labeled, X_unlabeled, n_components - len(np.unique(y_labeled)))
+        super().__init__(n_components, covariance_type="spherical", means_init=means_init)
+        self._initialize_parameters(X_unlabeled, np.random.mtrand._rand)
+        # force spherical covariance used by model
+        self.set_covariance(covariance)
+
+    def deep_e_step(self, X_unlabeled):
+        log_prob_norm, log_resp = self._estimate_log_prob_resp(X_unlabeled)
+        return np.exp(log_resp)
+
+    def deep_m_step(self, X_labeled, y_labeled, X_unlabeled, resp_unlabeled, covariance: float):
+        # calculate and concat inputs
+        resp_labeled = targets_to_resp(y_labeled, self.n_components)
+        X = np.vstack((X_labeled, X_unlabeled))
+        resp = np.vstack((resp_labeled, resp_unlabeled))
+        # update mixing weights and means, fixing covarinace before and after
+        self.set_covariance(covariance)
+        self.weights_, self.means_, self.covariances_ = gm._estimate_gaussian_parameters(
+            X, resp, self.reg_covar, self.covariance_type
+        )
+        self.weights_ /= self.weights_.sum()
+        self.set_covariance(covariance)
+
+    def set_covariance(self, covariance: float):
+        self.covariances_ = np.array([covariance] * self.n_components)
+        self.precisions_cholesky_ = gm._compute_precision_cholesky(
+            self.covariances_, self.covariance_type
+        )
+
+    def _e_step(self, X_unlabeled):
+        raise Exception("E-step disabled for DeepSSGMM, use SSGMMM for fitting")
+
+    def _m_step(self, X_unlabeled, log_resp):
+        raise Exception("M-step disabled for DeepSSGMM, use SSGMMM for fitting")
+
+
+def targets_to_resp(y_labeled, n_components):
+    n_labeled = len(y_labeled)
+    resp = np.zeros((n_labeled, n_components))
+    resp[np.arange(n_labeled), y_labeled] = 1
+    return resp
--- tests/test_ss_gmm.py
+++ tests/test_ss_gmm.py
@@ -3,7 +3,7 @@ import itertools
 import numpy as np
 
 import gcd_cluster.stats as stats
-from gcd_cluster.ss_gmm import SSGMM
+from gcd_cluster.ss_gmm import SSGMM, DeepSSGMM
 
 
 def test_ss_gmm():
@@ -27,6 +27,38 @@ def test_ss_gmm():
                        itertools.chain(range(n_labeled), range(n_clusters))]).flatten()
 
     ss_est = SSGMM(X_labeled, y_labeled, X_unlabeled, n_clusters).fit(X_unlabeled)
+    y_pred = ss_est.predict(np.vstack((X_labeled, X_unlabeled)))
+    row_ind, col_ind, weight = stats.assign_clusters(y_pred, y_true)
+    acc = stats.cluster_acc(row_ind, col_ind, weight)
+    assert acc == 1
+
+
+def test_deep_ss_gmm():
+    rng = np.random.default_rng(0)
+    # generate multivariate normal clusters
+    n_dim = 2
+    n_clusters = 10
+    n_samples = 100
+    n_labeled = 5
+    means = np.array([
+        [np.cos(2 * np.pi * i / 10), np.sin(2 * np.pi * i / 10)] for i in range(n_clusters)])
+    covariance = 1e-3
+    cov = np.eye(n_dim) * covariance
+    X_labeled = np.array([
+        rng.multivariate_normal(means[i], cov, n_samples) for i in range(n_labeled)
+        ]).reshape((-1, n_dim))
+    y_labeled = np.array([[i] * n_samples for i in range(n_labeled)]).flatten()
+    X_unlabeled = np.array([
+        rng.multivariate_normal(means[i], cov, n_samples) for i in range(n_clusters)
+        ]).reshape((-1, n_dim))
+    y_true = np.array([[i] * n_samples for i in
+                       itertools.chain(range(n_labeled), range(n_clusters))]).flatten()
+
+    ss_est = DeepSSGMM(X_labeled, y_labeled, X_unlabeled, n_clusters, covariance)
+    for _ in range(ss_est.max_iter):
+        print(ss_est.means_)
+        resp_unlabeled = ss_est.deep_e_step(X_unlabeled)
+        ss_est.deep_m_step(X_labeled, y_labeled, X_unlabeled, resp_unlabeled, covariance)
     y_pred = ss_est.predict(np.vstack((X_labeled, X_unlabeled)))
     row_ind, col_ind, weight = stats.assign_clusters(y_pred, y_true)
     acc = stats.cluster_acc(row_ind, col_ind, weight)
